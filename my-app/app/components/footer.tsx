360c15fb52c4578d059a34a35b7d8ddeeb727e7e
matthewbarnes1 <matthew.barnes35@gmail.com>
2024-03-25 20:12:29 -0700

    added stuff

--- my-app/app/components/footer.tsx
+++ my-app/app/components/footer.tsx
@@ -2,6 +2,11 @@ import Link from 'next/link'
 
 export default function Foot(){
     return <>
-    
+    {/* Contact info, page hyperlinks, and blog link */}
+    {/* Include
+    - Copyright ex: © 2024 Flourishing Fibers
+    - Contact info
+    - Blog link (learn how to incorporate this to maximize SEO outcomes)
+    */}
     </>
 }
--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -4,19 +4,18 @@ import './contact.css'
 export default function Page(){
     return <>
     <Head></Head>
-    <h1>This is the contact page</h1>
-
-    <div className="card">
+    <div className="wrap">
          {/* Two options, book directly through calendly, or contact using static # and email */}
         <div className="book">
-            <h2>Book a free consultation!</h2>
-            <p>Let's review your goals, and plan a strategic approach to reaching your full potential!</p>
+            <h2>Schedule a meeting</h2>
+            <p>Let's talk about your goals, and plan a strategic approach to reaching your full potential!</p>
+        {/* This will link to calendly page (or use api to embed directly into site both works) */}
         </div>
 
         <div className="contact">
             <h2>Contact us directly!</h2>
-            <p>Call: 647-999-9287</p>
-            <p>Email: team@flourishingfibers.ca</p>
+            <p>Call: 647-000-0000</p>
+            <a href="mailto:team@flourishingfibers.ca">Email: team@flourishingfibers.ca</a>
         </div>
     </div>
     </>
